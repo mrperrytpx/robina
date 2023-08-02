7186fa1a0052235c107f1e2244e6490f2cc2f61b
Tomislav <77341991+mrperrytpx@users.noreply.github.com>
2023-08-02 23:36:01 +0200

    Update regex.ts

--- src/util/regex.ts
+++ src/util/regex.ts
@@ -2,4 +2,4 @@ export const nword =
     /^.*([nN])+[\W_]*([iI&])+\s*[\W_]*([gG]\s*[\W_]*|[qQ]\s*[\W_]*){2,}\s*[\W_]*([aA]{1,}|([eErR]+[\W_]*\s*[eErR]+))/g;
 
 export const fword =
-    /^.*([fF])+[\w\W]*([aA&])+\s*[\w\W]*([gG]\s*[\w\W]*|[qQ]\s*[\w\W]*){2,}\s*[\w\W]*[oO0tT]+/g;
+    /^.*([fF])+[\w\W]*([aA&])+\s*[\w\W]*([gG]\s*[\w\W]*|[qQ]\s*[\w\W]*){2,}\s*[\w\W]*([oO0]+[\W_]*\s*[tT7]+)/g;
