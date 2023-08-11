0583b152a6f60cd41585ffe68be6f71f0d9abb1d
Tomislav <77341991+mrperrytpx@users.noreply.github.com>
2023-08-11 18:21:24 +0200

    Update ChatroomMessages.tsx

--- src/components/ChatroomMessages.tsx
+++ src/components/ChatroomMessages.tsx
@@ -122,7 +122,7 @@ export const ChatroomMessages = ({
             }
         );
 
-        while (queue.length > 0) {
+        while (queue.length > 0 && !postMessage.isLoading) {
             const message = queue.shift();
             if (!message) return;
 
